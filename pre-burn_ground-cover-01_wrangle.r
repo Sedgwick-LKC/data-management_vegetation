c336317e4f8a2639287f1485afacae4784732d5e
njlyon0 <nicholasjlyon@gmail.com>
2025-11-17 10:41:15 -0500

    feat (pre-burn): script skeleton for ground cover wrangling

---
+++ pre-burn_ground-cover-01_wrangle.r
@@ -0,0 +1,22 @@
+## ------------------------------------------------ ##
+# Pre-Burn Ground Cover - Wrangle
+## ------------------------------------------------ ##
+# Purpose:
+## Wrangle the 'raw' pre-burn ground cover data
+## Initial data structure inherited from WildNote app
+
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Get set up
+source("00_setup.r")
+
+# Clear environment
+rm(list = ls()); gc()
+
+## ----------------------------- ##
+# Load Data ----
+## ----------------------------- ##
+
+# End ----
