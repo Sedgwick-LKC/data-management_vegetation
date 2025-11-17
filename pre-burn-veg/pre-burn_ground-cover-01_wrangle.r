cc65367bd20fd61e9041bff2766f7a6a4c0c7bbc
njlyon0 <nicholasjlyon@gmail.com>
2025-11-17 10:55:15 -0500

    dev (pre-burn): partial effort at ingesting wildnote data

--- pre-burn-veg/pre-burn_ground-cover-01_wrangle.r
+++ pre-burn-veg/pre-burn_ground-cover-01_wrangle.r
@@ -7,7 +7,7 @@
 
 # Load libraries
 # install.packages("librarian")
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, readxl)
 
 # Get set up
 source("00_setup.r")
@@ -18,5 +18,28 @@ rm(list = ls()); gc()
 ## ----------------------------- ##
 # Load Data ----
 ## ----------------------------- ##
+
+# Define file path to desired data
+cov_file <- file.path("data", "raw", "Ground-Cover-Pre-Burn.xlsx")
+
+# Check existing sheets in the data
+(cov_sheets <- readxl::excel_sheets(path = cov_file))
+
+# Read in both
+cov_sheet1 <- readxl::read_excel(path = cov_file, sheet = cov_sheets[1], col_type = "text")
+cov_sheet2 <- readxl::read_excel(path = cov_file, sheet = cov_sheets[2], col_type = "text")
+
+# Check structure
+dplyr::glimpse(cov_sheet1)
+dplyr::glimpse(cov_sheet2)
+
+# Join by shared columns
+cov_v01 <- dplyr::left_join(x = cov_sheet2, y = cov_sheet1,
+  by = c("Survey ID", "Survey Date", "User"))
+
+# Check that out
+dplyr::glimpse(cov_v01)
+## tibble::view(cov_v01)
+
 
 # End ----
